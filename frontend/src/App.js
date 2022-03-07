180676800d73f908c7f794d1514dc4874ced8ef7
Aleksandra Loshkareva <tapokstayl@gmai;.com>
2022-03-07 15:55:40 +0700

    popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Map, { FullscreenControl, Marker, Popup } from 'react-map-gl';
-import { Card, CardContent, Typography, Box, Button, Grid } from '@mui/material';
+import { Typography, Button, Grid } from '@mui/material';
 import './Popup.css'
 
 
@@ -24,42 +24,88 @@ function App() {
     </Marker>
     <FullscreenControl />
     {showPopup && (
-      <Popup className="" longitude={84.94753445606905} latitude={56.46955924473702}
+      <Popup longitude={84.94753445606905} latitude={56.46955924473702}
         anchor="bottom"
         onClose={() => setShowPopup(false)}
+        maxWidth="400px"
       >
-
-        <Typography sx={{ fontSize: '24px', textAlign: 'center' }}>Зорина Алина Александровна</Typography>
-
         <Grid
           container
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          xs={12}
+          columns={12}
+          sx={{ fontSize: '14px', color: '#fff', textAlign: 'center' }}
+          spacing={2}
         >
+          <Grid item>
+            <Typography sx={{ fontSize: '24px' }} variant="h5">
+              Зорина Алина
+              Александровна
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            container
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
+            <Grid item xs={2} sm={4} md={4}>
+              <Typography sx={{ color: 'rgba(255, 255, 255, 0.75)', fontWeight: '400', fontStyle: 'normal' }} >выпуск</Typography>
+            </Grid>
+            <Grid item xs={2} sm={4} md={4}>
+              <Typography sx={{ color: 'rgba(255, 255, 255, 0.75)', fontWeight: '400', fontStyle: 'normal' }}>город</Typography>
+            </Grid>
+            <Grid item xs={2} sm={4} md={4}>
+              <Typography sx={{ color: 'rgba(255, 255, 255, 0.75)', fontWeight: '400', fontStyle: 'normal' }} >страна</Typography>
+            </Grid>
+            <Grid item xs={2} sm={4} md={4}>
+              <Typography sx={{ fontWeight: '500', fontStyle: 'normal' }}>2021</Typography>
+            </Grid>
+            <Grid item xs={2} sm={4} md={4} >
+              <Typography sx={{ fontWeight: '500', fontStyle: 'normal' }}>Клининград</Typography>
+            </Grid>
+            <Grid item xs={2} sm={4} md={4}>
+              <Typography sx={{ fontWeight: '500', fontStyle: 'normal' }} >Россия</Typography>
+            </Grid>
+          </Grid>
+          <Grid item container spacing={1}>
 
-          <Grid xs={4} items><Typography sx={{ fontSize: '14px' }}>выпуск</Typography></Grid>
-          <Grid xs={4} items> <Typography sx={{ fontSize: '14px' }}>город</Typography></Grid>
-          <Grid xs={4} items> <Typography sx={{ fontSize: '14px' }}>страна</Typography></Grid>
-          <Grid xs={4} items><Typography sx={{ fontSize: '14px' }}>2021</Typography></Grid>
-          <Grid xs={4} items> <Typography sx={{ fontSize: '14px' }}>Томск</Typography></Grid>
-          <Grid xs={4}items> <Typography sx={{ fontSize: '14px' }}>Россия</Typography></Grid>
+            <Grid xs={12} item>
+              <Grid item>
+                <Typography sx={{ color: 'rgba(255, 255, 255, 0.75)', fontWeight: '400', fontStyle: 'normal' }}>факультет</Typography>
+              </Grid>
+              <Grid item>
+                <Typography sx={{ fontWeight: 'bold', fontStyle: 'normal' }} >Физико-Технический факультет</Typography>
+              </Grid>
+            </Grid>
+
+            <Grid xs={12} item>
+              <Grid item>
+                <Typography sx={{ color: 'rgba(255, 255, 255, 0.75)', fontWeight: '400', fontStyle: 'normal' }}>работа</Typography>
+              </Grid>
+              <Grid item>
+                <Typography sx={{ fontWeight: 'bold', fontStyle: 'normal' }} >НИИПП, старший научный сотрудник </Typography>
+              </Grid>
+            </Grid>
+
+            <Grid xs={12} item>
+              <Grid item>
+                <Typography sx={{ color: 'rgba(255, 255, 255, 0.75)', fontWeight: '400', fontStyle: 'normal' }}>email</Typography>
+              </Grid>
+              <Grid item>
+                <Typography sx={{ fontWeight: 'bold', fontStyle: 'normal' }}>szorina@mail.ru</Typography>
+              </Grid>
+            </Grid>
+
+            <Grid xs={12} item>
+              <Grid item>
+                <Button sx={{ mt: 2 }} variant="contained">Статистика по городу</Button>
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
+      </Popup>)
+    }
 
-        {/* <Typography sx={{ fontSize: '14px' }}>выпуск</Typography>
-        <Typography sx={{ fontSize: '14px' }}>город</Typography>
-        <Typography sx={{ fontSize: '14px' }}>страна</Typography> */}
+  </Map >
 
-        {/* <Typography>2021</Typography>
-        <Typography>город Томск</Typography>
-        <Typography>страна Россия</Typography> */}
-        <Typography>факултет Физико-Технический факультет</Typography>
-        <Typography>работа НИИПП, старший научный сотрудник </Typography>
-        <Typography>email szorina@mail.ru</Typography>
-        <Button sx={{ backgroundColor: "#FFB35C", color: "#fff" }}>Статистика по городу</Button>
-      </Popup>)}
-  </Map>
 }
 
 export default App;
